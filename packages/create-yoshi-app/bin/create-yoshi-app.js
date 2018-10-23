8abb000e76c1430521888b9c0b63768b396df420
Ori Price <oripr@oripr-mbp15.local>
2018-10-23 13:55:06 +0300

    validate project name when creating yoshi app

--- packages/create-yoshi-app/bin/create-yoshi-app.js
+++ packages/create-yoshi-app/bin/create-yoshi-app.js
@@ -8,6 +8,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const program = require('commander');
 const chalk = require('chalk');
+const validateProjectName = require('validate-npm-package-name');
 const { createApp } = require('../src/index');
 const pkg = require('../package.json');
 
@@ -18,10 +19,40 @@ program
   .parse(process.argv);
 
 const customProjectDir = program.args[0];
+const workingDir = process.cwd();
+
+verifyDirectoryName(customProjectDir || workingDir);
 
 if (customProjectDir) {
   fs.ensureDirSync(customProjectDir);
   process.chdir(path.resolve(customProjectDir));
 }
 
-createApp(process.cwd(), customProjectDir);
+createApp(workingDir, customProjectDir);
+
+function printValidationResults(results) {
+  if (typeof results !== 'undefined') {
+    results.forEach(error => {
+      console.error(chalk.red(`  *  ${error}`));
+    });
+  }
+}
+
+function verifyDirectoryName(workingDir) {
+  const lastSegmentPath = workingDir.split(path.sep).slice(-1)[0];
+  console.log(lastSegmentPath);
+
+  const validationResult = validateProjectName(lastSegmentPath);
+
+  if (!validationResult.validForNewPackages) {
+    console.error(
+      `Could not create a project called ${chalk.red(
+        `"${lastSegmentPath}"`,
+      )} because of restrictions:`,
+    );
+
+    printValidationResults(validationResult.errors);
+    printValidationResults(validationResult.warnings);
+    process.exit(1);
+  }
+}
